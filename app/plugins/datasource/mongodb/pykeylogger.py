05e55da3fdbe2895662d3c3296081c3c51de7441
janis <janis>
2016-09-27 19:14:00 -0600

    pykeylogger mongo plug in

--- app/plugins/datasource/mongodb/pykeylogger.py
+++ app/plugins/datasource/mongodb/pykeylogger.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from core.apis.datasource.annotations import Annotations
+import ujson
 
 class PyKeyLogger:
 
@@ -20,3 +22,63 @@ class PyKeyLogger:
         collection = self.getDatabase().timed
         result = collection.insert_many(json)
         return len(result.inserted_ids)
+
+# Keypress #
+    # select data by date range of the 'start' column
+    def selectKeyPressData(self, startDate, endDate):
+        return 0;
+
+    # insert a new record.  This record must be tied to the original record.
+    # the oldDataId will be a new 'column' called sourceId. it is of type ObjectId
+    def insertFixedKeyPressData(self, oldDataId, json):
+        return 0
+
+    # update a previously 'fixed' record. Make sure that this record has a value in the sourceId.
+    # ORIGINAL DATA SHOULD NEVER BE UPDATED OR DELETED.
+    def updateFixedKeyPressData(self, dataId, json):
+        return 0
+
+    # delete a record.  Make sure that this record has a value in the sourceId.
+    # ORIGINAL DATA SHOULD NEVER BE UPDATED OR DELETED.
+    def deleteFixedKeyPressData(self, dataId):
+        return 0
+
+# Click #
+    # select data by date range of the 'start' column
+    def selectClickData(self, startDate, endDate):
+        return 0;
+
+    # insert a new record.  This record must be tied to the original record.
+    # the oldDataId will be a new 'column' called sourceId. it is of type ObjectId
+    def insertFixedClickData(self, oldDataId, json):
+        return 0
+
+    # update a previously 'fixed' record. Make sure that this record has a value in the sourceId.
+    # ORIGINAL DATA SHOULD NEVER BE UPDATED OR DELETED.
+    def updateFixedClickData(self, dataId, json):
+        return 0
+
+    # delete a record.  Make sure that this record has a value in the sourceId.
+    # ORIGINAL DATA SHOULD NEVER BE UPDATED OR DELETED.
+    def deleteFixedClickData(self, dataId):
+        return 0
+
+# Timed #
+    # select data by date range of the 'start' column
+    def selectTimedData(self, startDate, endDate):
+        return 0;
+
+    # insert a new record.  This record must be tied to the original record.
+    # the oldDataId will be a new 'column' called sourceId. it is of type ObjectId
+    def insertFixedTimedData(self, oldDataId, json):
+        return 0
+
+    # update a previously 'fixed' record. Make sure that this record has a value in the sourceId.
+    # ORIGINAL DATA SHOULD NEVER BE UPDATED OR DELETED.
+    def updateFixedTimedData(self, dataId, json):
+        return 0
+
+    # delete a record.  Make sure that this record has a value in the sourceId.
+    # ORIGINAL DATA SHOULD NEVER BE UPDATED OR DELETED.
+    def deleteFixedTimedData(self, dataId):
+        return 0
