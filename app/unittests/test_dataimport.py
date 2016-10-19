593afaf8bd4ee8320cc4c981081a94a8cba47e2a
janis <janis>
2016-10-18 19:56:41 -0600

    updated the dataimport tests with the new amounts int he new files

--- app/unittests/test_dataimport.py
+++ app/unittests/test_dataimport.py
@@ -1,44 +1,46 @@
 import unittest
-import datetime
+from datetime import datetime
 from core.config.dataImportConfig import DataImportConfig
 
 class DataImportTest(unittest.TestCase):
+    now = datetime.now()
+    nowString = datetime.strftime(now, '%Y-%m-%d %H:%M:%S')
 
-    def test_insertKeypressData(self):
-        size = DataImportConfig().importKeypressData("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 24)
+    def test_1insertKeypressData(self):
+        size = DataImportConfig().importKeypressData("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 46)
 
-    def test_importClick(self):
-        size = DataImportConfig().importClick("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 8)
+    def test_2importClick(self):
+        size = DataImportConfig().importClick("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 12)
 
-    def test_importTimed(self):
-        size = DataImportConfig().importTimed("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 21)
+    def test_3importTimed(self):
+        size = DataImportConfig().importTimed("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 34)
 
-    def test_importTsharkThroughput(self):
-        size = DataImportConfig().importTsharkThroughput("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 67)
+    def test_4importMultiExcludeThroughput(self):
+        size = DataImportConfig().importMultiExcludeThroughput("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 128)
 
-    def test_importMultiIncludeThroughput(self):
-        size = DataImportConfig().importMultiIncludeThroughput("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 77)
+    def test_5importMultiExcludeProtocol(self):
+        size = DataImportConfig().importMultiExcludeProtocol("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 128)
 
-    def test_importMultiExcludeThroughput(self):
-        size = DataImportConfig().importMultiExcludeThroughput("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 77)
+    def test_6importMultiIncludeThroughput(self):
+        size = DataImportConfig().importMultiIncludeThroughput("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 128)
 
-    def test_importMultiExcludeProtocol(self):
-        size = DataImportConfig().importMultiExcludeProtocol("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 77)
+    def test_7importMultiIncludeProtocol(self):
+        size = DataImportConfig().importMultiIncludeProtocol("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 128)
 
-    def test_importMultiIncludeProtocol(self):
-        size = DataImportConfig().importMultiIncludeProtocol("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 77)
+    def test_8importTsharkThroughput(self):
+        size = DataImportConfig().importTsharkThroughput("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 130)
 
-    def test_importTsharkProtocol(self):
-        size = DataImportConfig().importTsharkProtocol("Alex", "Super summer Event", "here are some comments", datetime.datetime.now())
-        self.assertEqual(size, 67)
+    def test_9importTsharkProtocol(self):
+        size = DataImportConfig().importTsharkProtocol("Alex", "Super summer Event", "here are some comments", self.nowString)
+        self.assertEqual(size, 130)
 
 
 if __name__ == '__main__':
