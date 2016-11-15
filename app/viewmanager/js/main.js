98f11df8e440d5495061a77f90516d378f4db8b5
janis <janis>
2016-11-14 21:37:07 -0700

    merge mark's and mine together

--- app/viewmanager/js/main.js
+++ app/viewmanager/js/main.js
@@ -16,7 +16,7 @@ $(document).on("click", "#dateInput", function(){
 	windowRangeEnd = end;
 	var techNames = $("#techOptions").val();
 	var eventNames = $("#eventOptions").val();
-   var eventTechNames = $("#techAndEventOptions").val();
+    var eventTechNames = $("#techAndEventOptions").val();
 
 	if(start == ""){
 		start = '2000-01-01 00:00:00';
@@ -61,57 +61,226 @@ $(document).on("change", "#snapshots", function(){
 var keylogger;
 var pcapData;
 var snap;
-function visualizeKeyData(keyData, clickData, timedData){
-	keyData.forEach(function(obj){
-		obj['type'] = ['box'];
-	});
-	clickData.forEach(function(obj){
-		obj['type'] = ['box'];
-	});
-	timedData.forEach(function(obj){
-		obj['type'] = ['box'];
-	});
-	keylogger = new KeyLogger(keyData, clickData, timedData);
+function visualizeKeyData(keyData, clickData, timedData, count){
+    eventTechNames = getArrayOfEventTechNames();
+    // if eventTechNames.length > 0 then we know we have multiple datasets to work with.
+    if(eventTechNames.length > 0) {
+        keyDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, keyData);
+        _.each(keyDataArrays, function(keyDataArray) {
+            _.each(keyDataArray['data'], function(obj) {
+                obj['type'] = ['box'];
+            });
+        });
+
+        clickDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, clickData);
+        _.each(clickDataArrays, function(clickDataArray) {
+            _.each(clickDataArray['data'], function(obj) {
+                obj['type'] = ['box'];
+            });
+        });
+
+        timedDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, timedData);
+        _.each(timedDataArrays, function(timedDataArray) {
+            _.each(timedDataArray['data'], function(obj) {
+                obj['type'] = ['box'];
+            });
+        });
+
+
+        _.each(eventTechNames, function(eventTechName, index) {
+            $("#keypressData").append(eventTechName);
+            keylogger = new KeyLogger(keyDataArrays[index]['data'], clickDataArrays[index]['data'], timedDataArrays[index]['data']);
+        });
+
+    } else {
+        // otherwise we have just one dataset
+        _.each(keyData, function(obj) {
+            obj['type'] = ['box'];
+        });
+        _.each(clickData, function(obj) {
+            obj['type'] = ['box'];
+        });
+        _.each(timedData, function(obj) {
+            obj['type'] = ['box'];
+        });
+
+        keylogger = new KeyLogger(keyData, clickData, timedData);
+	}
+
 }
 
 function visualizePCAPData(meXY, meAll, miXY, miAll, tsXY, tsAll){
-	meXY.forEach(function(xyObj){
-		var xyTime = xyObj['x'];
-		meAll.forEach(function(allObj){
-			var allTime = allObj['start'];
-			if(allTime == xyTime){
-				xyObj['label'] = {"content": allObj['content']};
-			}
-		});
-	});
-	miXY.forEach(function(xyObj){
-		var xyTime = xyObj['x'];
-		miAll.forEach(function(allObj){
-			var allTime = allObj['start'];
-			if(allTime == xyTime){
-				xyObj['label'] = {"content": allObj['content']};
-			}
-		});
-	});
-	tsXY.forEach(function(xyObj){
-		var xyTime = xyObj['x'];
-		tsAll.forEach(function(allObj){
-			var allTime = allObj['start'];
-			if(allTime == xyTime){
-				xyObj['label'] = {"content": allObj['content']};
-			}
-		});
-	});
-	pcapData = new PCAPData(meXY, meAll, miXY, miAll, tsXY, tsAll);
+    eventTechNames = getArrayOfEventTechNames();
+    // if eventTechNames.length > 0 then we know we have multiple datasets to work with.
+    if(eventTechNames.length > 0) {
+
+        // multi exclude data
+        meXYDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, meXY);
+        meAllDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, meAll);
+
+        _.each(meXYDataArrays, function(meXYDataArray, index) {
+            _.each(meXYDataArray['data'], function(xyObj) {
+                var xyTime = xyObj['x'];
+                _.each(meAllDataArrays[index]['data'], function(allObj) {
+                    var allTime = allObj['start'];
+                    if(allTime == xyTime){
+                        xyObj['label'] = {"content": allObj['content']};
+                    }
+                });
+            });
+        });
+
+        // multi include data
+        miXYDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, miXY);
+        miAllDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, miAll);
+
+        _.each(miXYDataArrays, function(miXYDataArray, index) {
+            _.each(miXYDataArray['data'], function(xyObj) {
+                var xyTime = xyObj['x'];
+                _.each(miAllDataArrays[index]['data'], function(allObj) {
+                    var allTime = allObj['start'];
+                    if(allTime == xyTime){
+                        xyObj['label'] = {"content": allObj['content']};
+                    }
+                });
+            });
+        });
+
+        // tshark data
+        tsXYDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, tsXY);
+        tsAllDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, tsAll);
+
+        _.each(tsXYDataArrays, function(tsXYDataArray, index) {
+            _.each(tsXYDataArray['data'], function(xyObj) {
+                var xyTime = xyObj['x'];
+                _.each(tsAllDataArrays[index]['data'], function(allObj) {
+                    var allTime = allObj['start'];
+                    if(allTime == xyTime){
+                        xyObj['label'] = {"content": allObj['content']};
+                    }
+                });
+            });
+        });
+
+        _.each(eventTechNames, function(eventTechName, index) {
+            $("#multiExcludeData").append(eventTechName);
+            $("#multiIncludeData").append(eventTechName);
+            $("#tsharkData").append(eventTechName);
+            pcapData = new PCAPData(meXYDataArrays[index]['data'], meAllDataArrays[index]['data'],
+                                    miXYDataArrays[index]['data'], miAllDataArrays[index]['data'],
+                                    tsXYDataArrays[index]['data'], tsAllDataArrays[index]['data']);
+        });
+
+
+    } else {
+        // otherwise we have just one dataset
+        _.each(meXY, function(xyObj) {
+            var xyTime = xyObj['x'];
+            _.each(meAll, function(allObj) {
+                var allTime = allObj['start'];
+                if(allTime == xyTime){
+                    xyObj['label'] = {"content": allObj['content']};
+                }
+            });
+        });
+
+        _.each(miXY, function(xyObj) {
+            var xyTime = xyObj['x'];
+            _.each(miAll, function(allObj) {
+                var allTime = allObj['start'];
+                if(allTime == xyTime){
+                    xyObj['label'] = {"content": allObj['content']};
+                }
+            });
+        });
+        _.each(tsXY, function(xyObj) {
+            var xyTime = xyObj['x'];
+            _.each(tsAll, function(allObj) {
+                var allTime = allObj['start'];
+                if(allTime == xyTime){
+                    xyObj['label'] = {"content": allObj['content']};
+                }
+            });
+        });
+        pcapData = new PCAPData(meXY, meAll, miXY, miAll, tsXY, tsAll);
+	}
 }
 
 function visualizeSnapshotData(snapData){
-	snapData.forEach(function(obj){
-		obj['type'] = ['box'];
-	});
-	snap = new Screenshot(snapData);
+    eventTechNames = getArrayOfEventTechNames();
+    // if eventTechNames.length > 0 then we know we have multiple datasets to work with.
+    if(eventTechNames.length > 0) {
+        snapDataArrays = splitDataForMultipleDataSetManagement(eventTechNames, snapData);
+        _.each(snapDataArrays, function(snapDataArray) {
+            _.each(snapDataArray['data'], function(obj) {
+                obj['type'] = ['box'];
+            });
+
+            $("#screenshotData").append(snapDataArray["eventTechName"]);
+            snap = new Screenshot(snapDataArray['data']);
+    	});
+    } else {
+        // otherwise we have just one dataset
+        _.each(snapData, function(obj) {
+            obj['type'] = ['box'];
+        });
+
+    	snap = new Screenshot(snapData);
+	}
+
 }
 
+function splitDataForMultipleDataSetManagement(eventTechNames, jsonObjects) {
+    var allDataArray = []
+
+    _.each(eventTechNames, function(eventTechName) {
+        dataArray = []
+        _.each(jsonObjects, function(obj) {
+            if(obj["metadata"]["eventName"] + " by " + obj["metadata"]["techName"] == eventTechName) {
+                dataArray.push(obj);
+            }
+        });
+        if(dataArray.length > 0) {
+            var newJson = {}
+            newJson["eventTechName"] = eventTechName;
+            newJson["data"] = dataArray;
+            allDataArray.push(newJson);
+        }
+    });
+
+    return allDataArray;
+}
+
+function getArrayOfEventTechNames() {
+    // we need to get an array of the different possible event/tech combinations that we will use to separate the
+    // returned data for multi dataset management.
+    // but only if there is more than one option.  If there is just one option (or none) all the data will show
+    // on one timeline.
+
+	var techNames = $("#techOptions").val();
+	var eventNames = $("#eventOptions").val();
+    var eventTechNames = $("#techAndEventOptions").val();
+    var eventTechs = [];
+
+    // #techAndEventOptions already as the unique list, just return it
+    if(eventTechNames.length > 1) {
+        return eventTechNames;
+
+    // If either eventNames or techNames is > 1 we have multiple options
+    } else if (eventNames.length > 1 || techNames.length > 1) {
+
+        _.each(techNames, function(tech) {
+            _.each(eventNames, function(event){
+                eventTechs.push(event + ' by ' + tech);
+            });
+        });
+        return eventTechs;
+
+    // otherwise return an empty array to indicate all data should be in one timeline.
+    } else {
+        return eventTechs;
+    }
+}
 
 function prettyAdd(title, callback){
 	swal({
@@ -193,4 +362,34 @@ $(document).ready(function() {
     $.get(getEventNames)
     var getEventTechNames = "http://localhost?populateDropdown=availableTechAndEventNames";
     $.get(getEventTechNames)
+});
+
+//Disables dropdown menu for events. Kept the plugin id updates ($("#techAndEventOptions").prop("disabled", false);)
+//seperate from the on.change (for #eventOptions) function above and the #techAndEventOptions on.change below
+//to prevent overwriting each other
+$(document).on("change", "#eventOptions", function(evt, params){
+	var eventNames = $("#eventOptions").val();
+	if(eventNames.length>0){
+		$("#techAndEventOptions").prop("disabled", true);
+	}
+	else{
+		$("#techAndEventOptions").prop("disabled", false);
+	}
+	$("#techAndEventOptions").trigger("chosen:updated");
+});
+
+
+$(document).on("change", "#techAndEventOptions", function(evt, params){
+	var techAndEventNames = $("#techAndEventOptions").val();
+	if(techAndEventNames.length>0){
+		$("#eventOptions").prop("disabled", true);
+		$("#techOptions").prop("disabled", true);
+	}
+	else{
+		$("#eventOptions").prop("disabled", false);
+		$("#techOptions").prop("disabled", false);
+	}
+	$("#eventOptions").trigger("chosen:updated");
+	$("#techOptions").trigger("chosen:updated");
+
 });
