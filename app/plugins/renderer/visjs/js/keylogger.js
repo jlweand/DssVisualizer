34f29ca0ceee942e552c3db20df4e966d3d96819
janis <janis>
2016-11-22 19:37:00 -0700

    added back the method name to the method call

--- app/plugins/renderer/visjs/js/keylogger.js
+++ app/plugins/renderer/visjs/js/keylogger.js
@@ -61,7 +61,7 @@ var KeyLogger = function(keyData, clickData, timedData){
 		},
 		onUpdate: function(item, callback){
 			// 	  $.get("http://localhost?submission=annotation&itemID="+currItem+"&type="+groupName+"&annotation="+value);
-			(item, callback);
+			prettyPrompt(item, callback);
 		},
 		onRemove: function(item, callback) {
             prettyConfirm('Remove item', 'Do you really want to remove item ' + item.content + '?', function (ok) {
